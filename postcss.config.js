d55c2d308cd558a98e1c2fad5e16d08c934cb693
Bladymir Tellez <btellez@gmail.com>
2020-03-29 19:03:01 -0500

    Add PostCSS and Fix Broken Image

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./src/index.html'],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    }),
+    require('postcss-nested')
+  ]
+};
